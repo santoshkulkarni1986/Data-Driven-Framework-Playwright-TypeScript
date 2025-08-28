9781d291428a095c5062e0b43c9134222cb8d20b
Santosh Kulkarni <santoshvqa@gmail.com>
2025-08-28 16:04:32 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     ['junit', { outputFile: 'FinalReports/test-results/results.xml' }],
     ['@estruyf/github-actions-reporter'],
     // Compiled JS reporters (dist folder)
-     ['./dist/Utility/PdfReporter.js', { outputFolder: 'FinalReports/reports/pdf' }],
+    ['ts-node/register', { require: './src/Utility/PdfReporter.ts', outputFolder: 'FinalReports/reports/pdf' }],
     [
       'monocart-reporter',
       {
